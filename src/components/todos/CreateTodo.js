6f69147d48b3bea63da1dd8ca4265b5eb87ef672
NickSpangler <Spangleyman@mac.com>
2020-12-10 10:55:48 -0500

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class CreateTodo extends Component {
+
+  state = {
+    input: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addTodo(this.state)
+    this.setState({
+      input: ''
+    })
+  }
+
   render() {
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label>Add Todo</label>
+            <input type='text' onChange={this.handleChange} value={this.state.input}/>
+          </p>
+          <input type='submit' />
+        </form>
       </div>
     )
   }
 }
 
-export default CreateTodo;
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: formData => dispatch({type: 'ADD_TODO', payload: formData})
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CreateTodo);
